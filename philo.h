a0f36ed419dd57b9a57b630cef49d26ed084e7db
pejasco <pejasco@gmail.com>
2024-05-01 10:21:46 +0100

    hey

--- philo.h
+++ philo.h
@@ -3,32 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: siev <siev@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: chuleung <chuleung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 18:27:48 by siev              #+#    #+#             */
-/*   Updated: 2024/04/30 19:16:23 by siev             ###   ########.fr       */
+/*   Updated: 2024/05/01 10:20:53 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 #define PHILO_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> 
+//#include <stdio.h> //printf
+//#include <stdlib.h> //malloc free
+//#include <unistd.h> //write, usleep
+#include <limits.h> //INT_MAX
 #include <pthread.h>
-#include <sys/time.h>
-#include <sys/types.h>
+//#include <sys/time.h> //gettimeofday
+//#include <sys/types.h> uni_int64_t
+
+typedef pthread_mutex_t	t_mutex;
+
+typedef struct s_fork
+{
+	t_mutex	fork;
+	int		fork_id;
+} t_fork;
 
 typedef struct s_philo
 {
-    int 
-
-
-
-
-}
-
+	int			id;
+	t_fork		*left_fork;
+	t_fork		*right_fork;		
+} t_philo;
 
 
 
